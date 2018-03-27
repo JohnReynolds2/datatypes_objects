17bda6ffa2f4d85e872493f97ad4b346f91b7cf1
SpanishJoplin <jpreynoldsku@gmail.com>
2018-03-27 05:41:01 -0600

    Commit 1-selecting datatypes

---
+++ datatypes_objects.js
@@ -0,0 +1,8 @@
+//Commit 1 - Selecting datatypes
+//1. Boolean , on === true, off === false
+//2. array , [eamil1, eamil2, email3, email4]
+//3. objects  , const spaceship = {hull: , laser blasters:, tractorbeam:, warpdrive: }
+//4. array ,  class = [student1, student2, student3, student4]
+//5. objects , const class = {student1 = {location}, student2 = {location}, student3 = {location}}
+//6. objects , const class = {student1 = {location, list of tv shows}, student2 = {location, list of tv shows}, student3 = {location, list of tv shows}}
+
