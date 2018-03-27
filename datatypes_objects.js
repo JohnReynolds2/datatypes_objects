9110e915b48d0d713a7a4df128e8916a73dec4e8
SpanishJoplin <jpreynoldsku@gmail.com>
2018-03-27 05:47:01 -0600

    Commit 2-Created some data structures and accessed some data

--- datatypes_objects.js
+++ datatypes_objects.js
@@ -6,3 +6,17 @@
 //5. objects , const class = {student1 = {location}, student2 = {location}, student3 = {location}}
 //6. objects , const class = {student1 = {location, list of tv shows}, student2 = {location, list of tv shows}, student3 = {location, list of tv shows}}
 
+//Commit 2 - Created some data structures and accessed some data
+//1. array that holds the rainbow
+let rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+//2. code that accesses blue
+console.log(rainbow[4]);
+//3. object with my info
+const johnPaul = {
+	location: "Denver",
+	favoriteFood: "pizza",
+	hobby: "getting outside",
+	favoriteDatatype: "objects"
+};
+//4. access hobby
+console.log(johnPaul.hobby);
